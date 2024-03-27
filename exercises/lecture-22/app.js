3fc4655d4011548b142b612c6ed39ff5544cb1a2
Irina77777777 <drdpi@ukr.net>
2024-03-27 09:33:35 +0200

    add lesson-22 one

--- exercises/lecture-22/app.js
+++ exercises/lecture-22/app.js
@@ -1,7 +1,54 @@
 const classes = ["first", "second", "third", "fourth"];
 
-  let h1 = document.getElementsByTagName(h1);
-  console.log("h1", h1);
-  h1 = document.querySelector("h1");
-  console.log(h1);
-  console.dir(h1);
+   // task - 1
+   let hh1 = document.getElementsByTagName('h1');
+    console.log("h1", hh1);
+console.dir(hh1);
+console.log(hh1.nodeType);
+    for (i = 0; i < hh1.length; i++) {
+        console.log(hh1[i]);
+}
+    
+// task -2
+    let elem = document.querySelector('p');
+console.log(elem);
+elem.style.backgroundColor = 'gold';
+
+// task - 3
+let elem2 = document.querySelector('#p2');
+elem2.style.backgroundColor = 'gold';
+elem2.style.color = 'blue';
+elem2.style.fontSize = '2rem';
+
+// task - 4
+let elem3 = document.querySelector('p3');
+elem3.className = 'third';
+
+// task - 5
+let elem4 = document.querySelector("p4");
+elem4.className = 'fourth border';
+
+//task - 6
+const farClass = document.getElementsByClassName("container");
+console.log("Class:  ", farClass);
+for (const child of farClass) {
+    console.log( document.farClass.firstElementChild );
+}
+
+// task - 7
+    for (const child of farClass) {
+          console.log( document.farClass.firstElementChild.textContent );
+    }
+
+//task - 8
+let headers = document.querySelectorAll('.container header');
+for (i = 2; i < headers.length; i++) {
+     headers[i].setAttribute("nameTage", "h[i]");
+}
+//const classes = ["first", "second", "third", "fourth"];
+for (i = 0; i < headers.length; i++) {
+     headers[i].classList.add(classes[i]);
+}
+
+
+  
