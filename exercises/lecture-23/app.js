09584bb8a61f07b26d9a832a37697b8c472bcc3f
Irina77777777 <drdpi@ukr.net>
2024-03-31 11:45:29 +0300

    lesson-26 one

--- exercises/lecture-23/app.js
+++ exercises/lecture-23/app.js
@@ -1,53 +1,57 @@
 const alert = document.querySelector(".alert");
 // task - 1
-document.getElementsByClassName('btn-primary').onclick = function () {
+document.getElementsByClassName('.btn-primary').onclick = function () {
      alert.classList.add("alert-primary");
      alert.textContent = "A simple primary alert—check it out!";
 };
  
 // task - 2
-document.getElementsByClassName(" btn-secondary").onclick = function () {
+let cli = document.getElementsByClassName(" .btn-secondary");
+    cli.addEventListener('click', () => {
     alert.classList.add(" alert-primary");
     alert.textContent = "A simple secondary alert—check it out!";
-};
+});
 
 // task - 3
-document.getElementsByClassName("btn-success").mouseover = function () {
+let mouOver = document.getElementsByClassName(".btn-success");
+    mouOver.addEventListener('mouseover', () =>  {
     alert.classList.add(" alert-success");
     alert.textContent = "A simple success alert—check it out!";
-};
-document.getElementsByClassName("btn-success").mouseout = function () {
+    });
+
+     mouOver.addEventListener('mouseout ', () =>  {
     alert.classList.remove(" alert-success");
     alert.textContent = "  ";
-};
+});
 
 // task - 4
-document.getElementsByClassName("btn-danger").focus = function () {
+let foc = document.getElementsByClassName(".btn-danger")
+foc.addEventListener('focus', () =>  {
     alert.classList.add(" alert-danger");
     alert.textContent = "A simple danger alert—check it out!";
-};
-document.getElementsByClassName("btn-danger").focusout = function () {
+});
+foc.addEventListener('focusout', () =>  {
     alert.classList.remove(" alert-danger");
     alert.textContent = "  ";
-};
+});
 
 // task - 5
-document.getElementsByClassName("btn-dark  btn-light") = function toggleMode() {
+document.getElementsByClassName(".btn-dark  .btn-light") = function toggleMode() {
     alert.classList.add(" alert-danger");
     alert.textContent = "A simple danger alert—check it out!";
 };
-document.getElementsByClassName("btn-dark  btn-light") = function () {
+document.getElementsByClassName(".btn-dark  .btn-light") = function () {
     alert.classList.remove(" alert-danger");
     alert.textContent = "  ";
 };
 
 // task - 6
-document.getElementsByClassName("btn-info").keypress = function () {
+document.getElementsByClassName(".btn-info").keypress = function () {
     //  event.preventDefault()
     alert.classList.add(" alert-info");
     alert.textContent = "A simple info alert—check it out!";
 };
-document.getElementsByClassName("btn-info").focusout = function () {
+document.getElementsByClassName(".btn-info").focusout = function () {
     alert.classList.remove(" alert-danger");
     alert.textContent = "  ";
 };
@@ -61,7 +65,7 @@ document.getElementsByClassName("btn-info").focusout = function () {
 // task - 8
 const allCard= document.querySelectorAll("card");
 for (i = 0; i < allCard.length; i++) {
-    document.getElementsByClassName('add-to-cart').onclick = function () {
-        console.log(document.getElementsByClassName('card-title')[i]);
+    document.getElementsByClassName('.add-to-cart').onclick = function () {
+        console.log(document.getElementsByClassName('.card-title')[i]);
     };
 }
---
+++ exercises/lecture-26/app.js
@@ -0,0 +1,84 @@
+  // task - 1  -
+function calculate(numbers) {
+    let sum = 0;
+    for (const number of numbers) {
+        sum = sum + number;
+    } 
+    return sum;
+}
+calculate([1, 2, 4]); // => 7
+
+// sum() - це функція, яка описує операцію додавання.
+function sum(numbers) {
+    let nn = 0;
+    for (const number of numbers) {
+         nn = number + nn; 
+    }  return nn;  console.log(nn);
+}
+
+// multiply() - це функція, яка описує операцію множення.
+
+function multiply(numbers) {
+    let nn = 1;
+    for (const number of numbers) {
+        nn = number * nn;
+    }   return nn;
+}
+
+function calculate2(type, n, numbers) {
+    if (type === "sum") {
+            sum(numbers);
+        }  else if (type === "multiply") {
+            multiply(numbers);
+        }
+}
+calculate2(sum, 1, [1, 2, 4]);
+calculate2(multiply, 1, [1, 2, 4]); 
+
+// task - 2 - 
+let student_names = ["Wick", "Malcolm", "Smith"];
+const st2 = student_names.map(
+    (value, index, student_names) => (
+        "name :" + value , "index  " + index, "array: [" + student_names
+    )
+);
+console.log(st2);
+
+// task - 3 
+​let students_information = [
+    {"name": "Wick", "degree": 375}, 
+    {"name": "Malcolm", "degree": 405}, 
+    {"name": "Smith", "degree": 453},
+];
+const st3 = students_information.map(({ name, degree , percentage}) => ({
+    [name]: degree , percentage:(6000/degree) 
+}))  
+console.log(st3);
+
+// task -4
+const arr =
+[1, 2, 3, [10, 11, 12], 21, 22, 23, [31, 32, 33, 34], [41, 42]];
+
+const res = arr.reduce((current) => (res.push(current)));
+console.log(res);
+
+// task - 5
+function myFunc() {
+    let arr = ["Algorithm", "Data Structure", "Operating System", "html"];
+    arr.upperCase();
+    console.log(arr);
+}
+
+myFunc(); // [ 'ALGORITHM', 'DATA STRUCTURE', 'OPERATING SYSTEM', 'HTML' ]
+
+Array.prototype.upperCaseCase = function () {
+ for (let i = 0; i < this.length; i++) {
+ this[i] = this[i].toupperCase();
+ }
+};
+function myFunc() {
+ let arr3 = ["Algorithm", "Data Structure", "Operating System", "html"];
+ arr3.upperCase();
+ console.log(arr3);
+}
+myFunc();
